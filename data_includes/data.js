a464f9bd599f16a6d1a83422f5bbf9c20634ea6a
ezaroukian <egz@jhu.edu>
2015-03-06 11:32:40 +0000

    Update data.js

--- data_includes/data.js
+++ data_includes/data.js
@@ -6,11 +6,11 @@ var manualSendResults = false;
 var shuffleSequence = seq(rshuffle(rshuffle(
 "11-11n13_o_2B_can","13-11n13_o_2B_can","31-11n13_r_2B_isn","33-11n13_r_2B_isn",
 "11-13n11_r_2B_can","13-13n11_r_2B_can","31-13n11_o_2B_dif","33-13n11_o_2B_dif",
-//"11-31n11_o_2P","13-31n11_r_2P","31-31n11_r_2P","33-31n11_o_2P",
-//"11-11n31_r_2P","13-11n31_o_2P","31-11n31_o_2P","33-11n31_r_2P",
-//"11-1xn3x_u_2P2B","13-1xn3x_u_2P2B",
-//"31-x1nx3_u_2B2P","33-x1nx3_u_2B2P",
-//"11-x3nx1_o_2B2P","31-x3nx1_r_2B2P",
+"11-31n11_o_2P_isn","13-31n11_r_2P_dif","31-31n11_r_2P_dif","33-31n11_o_2P_isn",
+"11-11n31_r_2P_isn","13-11n31_o_2P_can","31-11n31_o_2P_can","33-11n31_r_2P_isn",
+"11-1xn3x_u_2P2B_dif","13-1xn3x_u_2P2B_dif",
+"31-x1nx3_u_2B2P_can","33-x1nx3_u_2B2P_can",
+"11-x3nx1_o_2B2P_dif","31-x3nx1_r_2B2P_dif",
 "13-3xn1x_o_2P2B_can","33-3xn1x_r_2P2B_isn"
   )));
 
