de77cecf389957eec0f983c509a94b458b40b67a
ezaroukian <egz@jhu.edu>
2015-05-19 16:04:46 -0400

    iii

--- data_includes/data.js
+++ data_includes/data.js
@@ -27,6 +27,10 @@ var defaults = [
   "Question", {
     randomOrder: false
   },  
+  "PracticeQuestion", {
+    hideProgressBar: true,
+	countsForProgressBar: false,
+  },
   "Message", {
     hideProgressBar: true
   },
@@ -55,84 +59,56 @@ var items = [
         q: "the person Mary reads the book Moby-Dick.<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "Yes",
         html: {include: "gen_instructions2.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
+
     }],
 	["inst", "SeparatorHTML", {
-        errorMessage: '<h2 style="color:red">Incorrect!</h2><p> The rule was "the person Mary reads the book Moby-Dick".</p><p> In the diagram, Mary reads Moby-Dick</p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
-
+        errorMessage: '<h2 style="color:red">Incorrect!</h2> <p> The rule was "the person Mary reads the book Moby-Dick".</p><p> In the diagram, Mary reads Moby-Dick</p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
     }],    
 	
     ["inst", "PracticeQuestion", {
         q: "the person Mary reads the book Middlemarch.<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "No",
         html: {include: "gen_instructions3.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
     }],
 	["inst", "SeparatorHTML", {
-        transfer: "keypress",
-        normalMessage: "Correct!",
-        errorMessage: '<p style="color:red">Incorrect!</p><p> The rule was "the person Mary reads the book Middlemarch".</p><p> In the diagram, Mary reads Moby-Dick, not Middlemarch.</p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
-		hideProgressBar: true,
-        countsForProgressBar: false,
+        errorMessage: '<h2 style="color:red">Incorrect!</h2> <p> The rule was "the person Mary reads the book Middlemarch".</p><p> In the diagram, Mary reads Moby-Dick, not Middlemarch.</p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
     }], 
 	
     ["inst", "PracticeQuestion", {
         q: "if (the person P1 reads the book B1) then (the person P1 has as name Mary).<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "Yes",
         html: {include: "gen_instructions4.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
     }],
 	["inst", "SeparatorHTML", {
-        transfer: "keypress",
-        normalMessage: "Correct!",
-        errorMessage: '<p style="color:red">Incorrect!</p><p> The rule was "if (the person P1 reads the book B1) then (the person P1 has as name Mary)".</p><p> In the diagram, there is one person-book pair we need to look at. <ol><li>When P1 = Mary and B1 = Moby-Dick, the rule requires B1 = Moby-Dick. - Consistent with rule.</li> </ol></p>  <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
-		hideProgressBar: true,
-        countsForProgressBar: false,
+        errorMessage: '<h2 style="color:red">Incorrect!</h2><p> The rule was "if (the person P1 reads the book B1) then (the person P1 has as name Mary)".</p><p> In the diagram, there is one person-book pair we need to look at. <ol><li>When P1 = Mary and B1 = Moby-Dick, the rule requires B1 = Moby-Dick. - Consistent with rule.</li> </ol></p>  <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
     }], 
 	
     ["inst", "PracticeQuestion", {
         q: "if (the person P1 reads the book B1) then (the person P1 has as name Mary).<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "No",
         html: {include: "gen_instructions5.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
     }],
 	["inst", "SeparatorHTML", {
-        transfer: "keypress",
-        normalMessage: "Correct!",
-        errorMessage: '<p style="color:red">Incorrect!</p><p> The rule was "if (the person P1 reads the book B1) then (the person P1 has as name Mary)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = John and B1 = War and Peace, the rule requires P1 = Mary. - Inconsistent with rule!</li><li>When P1 = Mary and B1 = Moby-Dick, the rule requires P1 = Mary. - Consistent with rule. </li> </ol></p>  <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png" height="360">',
-		hideProgressBar: true,
-        countsForProgressBar: false,
+        errorMessage: '<h2 style="color:red">Incorrect!</h2><p> The rule was "if (the person P1 reads the book B1) then (the person P1 has as name Mary)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = John and B1 = War and Peace, the rule requires P1 = Mary. - Inconsistent with rule!</li><li>When P1 = Mary and B1 = Moby-Dick, the rule requires P1 = Mary. - Consistent with rule. </li> </ol></p>  <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png" height="360">',
     }], 
 	
 	["inst", "PracticeQuestion", {
         q: "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick).<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "No",
         html: {include: "gen_instructions6.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
     }],
 	["inst", "SeparatorHTML", {
-        transfer: "keypress",
-        normalMessage: "Correct!",
-        errorMessage: '<p style="color:red">Incorrect!</p><p> The rule was "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = John and B1 = War and Peace, B1 = Moby-Dick. - Inconsistent with rule!</li><li>When P1 = Mary and B1 = Moby-Dick, B1 = Moby-Dick. - Consistent with rule.</li> </ol></p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png" height="360">',
-		hideProgressBar: true,
-        countsForProgressBar: false,
+        errorMessage: '<h2 style="color:red">Incorrect!</h2><p> The rule was "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = John and B1 = War and Peace, B1 = Moby-Dick. - Inconsistent with rule!</li><li>When P1 = Mary and B1 = Moby-Dick, B1 = Moby-Dick. - Consistent with rule.</li> </ol></p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png" height="360">',
+
     }],
 	
 	["inst", "PracticeQuestion", {
         q: "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick).<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/3_zpskhezeis8.png' height='360'><br>Is the diagram consistent with the rule?",
         as: ["Yes","No"], hasCorrect: "Yes",
         html: {include: "gen_instructions6.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
     }],	
 	["inst", "SeparatorHTML", {
-        normalMessage: "Correct!",
-        errorMessage: '<p style="color:red">Incorrect!</p><p> The rule was "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = Mary and B1 = Moby-Dick, B1 = Moby-Dick. - Good</li> <li>When P1 = John and B1 = Moby-Dick, B1 = Moby-Dick. - Good</li></ol></p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/3_zpskhezeis8.png" height="360">',
+        errorMessage: '<h2 style="color:red">Incorrect!</h2><p> The rule was "if (the person P1 reads the book B1) then (the book B1 has as name Moby-Dick)".</p><p> In the diagram, there are two person-book pairs we need to look at. <ol><li>When P1 = Mary and B1 = Moby-Dick, B1 = Moby-Dick. - Good</li> <li>When P1 = John and B1 = Moby-Dick, B1 = Moby-Dick. - Good</li></ol></p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/3_zpskhezeis8.png" height="360">',
     }],
 	
     ["inst", "Message", {
