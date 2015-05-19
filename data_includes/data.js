c9d7c3c8920f87e9f7806f80bbb66feecc91ff14
ezaroukian <egz@jhu.edu>
2015-05-19 14:26:44 -0400

    Give correct

--- data_includes/data.js
+++ data_includes/data.js
@@ -78,7 +78,7 @@ var items = [
     }],
 	["inst", "PracticeQuestion", {
         q: "if (the person P1 reads the book B1) then (the person P1 has as name Mary).<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/2_zpstsuxtudr.png' height='360'><br>Is the diagram consistent with the rule?",
-        as: ["Yes","No"],
+        as: ["Yes","No"], hasCorrect: "No",
         html: {include: "gen_instructions6.html"},
         hideProgressBar: true,
         countsForProgressBar: false,
