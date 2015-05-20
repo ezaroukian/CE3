e143befdec730662bc15ea85e8bda1bc88199857
ezaroukian <egz@jhu.edu>
2015-05-20 08:21:46 -0400

    Change polarity

--- js_includes/genData.js
+++ js_includes/genData.js
@@ -315,8 +315,7 @@ function genInst(){
 
 	function frameEx(rule,link,test){
 		frame = rule+"<br/><img src='"+link+"' height='360'>";
-		if(!test){
-			frame += "<br>Is the diagram consistent with the rule?";}
+		if(!test){ frame += "<br>Is the diagram consistent with the rule?"; }
 		return frame;
 	}
 
@@ -339,14 +338,11 @@ function genInst(){
         html: {include: "gen_instructions2.html"},
     }],
 	["inst", "MySeparator", {
-        q: frameEx(ex1,ex1img,false),
+        q: frameEx(ex1,ex1img,true),
         as: ["Yes","No"], hasCorrect: "Yes",
         //html: {include: "gen_instructions2.html"},
 		errorMessage: '<h2 style="color:red">Incorrect!</h2> <p> The rule and diagram are repeated below.</p><p> In the diagram, Mary reads Moby-Dick.</p>',
-    }],
-//	["inst", "SeparatorHTML", {
-//        errorMessage: '<h2 style="color:red">Incorrect!</h2> <p> The rule was "the person Mary reads the book Moby-Dick".</p><p> In the diagram, Mary reads Moby-Dick</p> <img src="http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png" height="360">',
-//    }],    
+    }],    
 	
     ["inst", "PracticeQuestion", {
         q: "the person Mary reads the book Middlemarch.<br><img src='http://i1341.photobucket.com/albums/o753/ezaroukian/1_zpsitpxpxiy.png' height='360'><br>Is the diagram consistent with the rule?",
